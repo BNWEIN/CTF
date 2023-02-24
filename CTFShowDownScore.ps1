e3ba0642153d7967a70e2af641aad66d56c2dbd1
BNWEIN <bweinberg@prime-networks.co.uk>
2023-02-24 10:40:55 +0000

    Update CTFShowDownScore.ps1

--- CTFShowDownScore.ps1
+++ CTFShowDownScore.ps1
@@ -2,6 +2,7 @@ $challenge1StartDate = Get-Date "2023-02-14 11:00:00"
 $challenge2StartDate = Get-Date "2023-02-16 11:00:00"
 $challenge3StartDate = Get-Date "2023-02-20 12:00:00"
 $challenge4StartDate = Get-Date "2023-02-23 18:00:00"
+$challenge5StartDate = Get-Date "2023-02-23 18:00:00"
 
 # Fetch the HTML content of the website
 $url = "https://ctf.cyberdrain.com/showdownscore"
@@ -17,11 +18,13 @@ $csv = $table.Rows | Select-Object -Skip 1 | ForEach-Object {
     $challenge2 = if ($details -match "Challenge 2: (\S+)") { [datetime]::ParseExact($Matches[1], "yyyy-MM-ddTHH:mm:ss", $null) } else { $null }
     $challenge3 = if ($details -match "Challenge 3: (\S+)") { [datetime]::ParseExact($Matches[1], "yyyy-MM-ddTHH:mm:ss", $null) } else { $null }
     $challenge4 = if ($details -match "Challenge 4: (\S+)") { [datetime]::ParseExact($Matches[1], "yyyy-MM-ddTHH:mm:ss", $null) } else { $null }
+    $challenge5 = if ($details -match "Challenge 5: (\S+)") { [datetime]::ParseExact($Matches[1], "yyyy-MM-ddTHH:mm:ss", $null) } else { $null }
 
     $challenge1TimeTaken = if ($challenge1) { (New-TimeSpan $challenge1StartDate $challenge1) } else { New-TimeSpan }
     $challenge2TimeTaken = if ($challenge2) { (New-TimeSpan $challenge2StartDate $challenge2) } else { New-TimeSpan }
     $challenge3TimeTaken = if ($challenge3) { (New-TimeSpan $challenge3StartDate $challenge3) } else { New-TimeSpan }
     $challenge4TimeTaken = if ($challenge4) { (New-TimeSpan $challenge4StartDate $challenge4) } else { New-TimeSpan }
+    $challenge5TimeTaken = if ($challenge5) { (New-TimeSpan $challenge5StartDate $challenge4) } else { New-TimeSpan }
 
     [PSCustomObject]@{
         Score = $_.Cells[1].InnerText
@@ -30,7 +33,8 @@ $csv = $table.Rows | Select-Object -Skip 1 | ForEach-Object {
         "Challenge 2" = if ($challenge2) { $challenge2TimeTaken.ToString() } else { "" }
         "Challenge 3" = if ($challenge3) { $challenge3TimeTaken.ToString() } else { "" }
         "Challenge 4" = if ($challenge4) { $challenge4TimeTaken.ToString() } else { "" }
-        "Total Time" = ($challenge1TimeTaken + $challenge2TimeTaken + $challenge3TimeTaken + $challenge4TimeTaken).ToString()
+        "Challenge 5" = if ($challenge5) { $challenge5TimeTaken.ToString() } else { "" }
+        "Total Time" = ($challenge1TimeTaken + $challenge2TimeTaken + $challenge3TimeTaken + $challenge4TimeTaken + $challenge5TimeTaken).ToString()
     }
 }
 
